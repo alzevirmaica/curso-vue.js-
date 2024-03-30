1d578ad2d2a5bcbedeb460f2a1bcdd26a50e5eb2
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2024-03-30 00:03:55 -0300

    estudos sobre getters no vuex

--- vuex/emergenciasmedicas/src/components/ListaItens.vue
+++ vuex/emergenciasmedicas/src/components/ListaItens.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
+    <div v-if="tipo == 'socorristas'">
+      <select class="form-control form-control-sm" v-model="turno">
+        <option value="">Todos</option>
+        <option value="manhã">Manhã</option>
+        <option value="tarde">Tarde</option>
+        <option value="noite">Noite</option>
+      </select>
+    </div>
     <item
       v-for="(item, indice) in itens"
     :key="indice"
     :dados="item"
     />
+    <div v-if="tipo == 'socorristas'">
+      Total: {{ totalSocorristasPorTurno(turno) }}
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 import Item from "@/components/ItemDetalhe.vue";
 
 export default {
@@ -17,23 +29,38 @@ export default {
   components: {
     Item,
   },
+  data: () => ({
+    turno: ''
+  }),
   props: {
     tipo: String,
   },
   computed: {
     ...mapState({
-      enfermeiros: (state) => state.enfermeiros,
-      socorristas: (state) => state.socorristas,
-      medicos: (state) => state.medicos,
+      enfermeiros: state => state.enfermeiros,
+      medicos: state => state.medicos,
+      carros: state => state.equipamentos.carros,
+      telefones: state => state.equipamentos.telefones,
+      kitsDeReanimacao: state => state.equipamentos.kitsDeReanimacao
+    }),
+    ...mapGetters({
+      socorristasPorTurno: 'socorristasPorTurno',
+      totalSocorristasPorTurno: 'totalSocorristasPorTurno'
     }),
     itens() {
       switch (this.tipo) {
         case "enfermeiros":
           return this.enfermeiros;
         case "socorristas":
-          return this.socorristas;
+          return this.$store.getters.socorristasPorTurno(this.turno);
         case "medicos":
           return this.medicos;
+        case "carros":
+          return this.carros;
+        case "telefones":
+          return this.telefones;
+        case "kits-de-reanimacao":
+          return this.kitsDeReanimacao;
         default:
           return null;
       }
